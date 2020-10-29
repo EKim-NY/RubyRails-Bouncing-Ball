aa910c568f6f21bbb392b14969f8158439f1d621
Eren Kim <erenkim@gmail.com>
2020-10-28 23:25:53 -0400

    My first RubyRails/Vue project.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="current-id">
+    <h1>Hi Veronica! Welcome to the viva-LA-vue Workshop!</h1>
+    <div> I am a developer named {{ firstName }} {{ lastName }}. Watch me as I code in Vue.js! </div>
+    <p>
+      My First Name is: <input type="text" v-model="firstName">
+      <br>
+      My Last Name: <input type="text" v-model="lastName">
+    </p>
+    <div>
+      <button @click="toggleMyBoolean()">Toggle Me!</button> This is our boolean: {{ myBoolean}}
+    </div>
+    <div style="display:flex; align-items: center; justify-content: center;">
+      <bouncing-ball ballcolor="red"></bouncing-ball>
+       <bouncing-ball></bouncing-ball>
+      <bouncing-ball ballcolor="rgb(255,255,0)"></bouncing-ball>
+      <bouncing-ball ballcolor="#ADFF2F"></bouncing-ball>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import BouncingBall from "./components/BouncingBall.vue"
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data(){
+      return {
+        firstName: 'Eren',
+        lastName: 'Kim',
+        myBoolean: true
+      }
+    },
+    methods: {
+      toggleMyBoolean(){
+        this.myBoolean = !this.myBoolean
+      }
+    },
+    components: {
+      BouncingBall
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #current-id{
+    font-family: Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #42b883;
+    margin-top: 60px;
+  }
 </style>
